b4efb443de18856d2b3c8fc3cbd719969884b5a7
Balin Warren <balin.warren@gmail.com>
2024-11-13 17:23:37 -0700

    Added connection to mongodb

--- summaryScripts/main.py
+++ summaryScripts/main.py
@@ -1,4 +1,4 @@
-import pymongo
+from pymongo import MongoClient
 import mysql.connector
 from environs import Env
 
@@ -8,7 +8,7 @@ env.read_env()
 
 cluster = env.str("CLUSTER_IP")
 
-mongo_port = env.str("MONGO_URI")
+mongo_uri = env.str("MONGO_URI")
 
 
 mysql_port = env.str("MYSQL_PORT")
@@ -16,4 +16,19 @@ mysql_user = env.str("MYSQL_USER")
 mysql_pw = env.str("MYSQL_PW")
 
 # TODO GET COLLECTIONS
+mongo = MongoClient(mongo_uri)
+mongo_db = mongo["local"]
+collections = mongo_db.list_collection_names()
+print(collections)
 
+# TODO build summaries for all entries and just fraud
+
+# total of each card type JSON
+# total of each entry mode JSON
+# average amount float
+# total of each transaction type JSON
+# total country of transaction JSON
+# total shipping address countries  JSON
+# total Country of residence JSON
+# total of each bank JSON
+# fraud count
