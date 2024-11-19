17c1c5ea3209bd1f95f17d3187ff84475f3ca89e
Balin Warren <balin.warren@gmail.com>
2024-11-19 11:46:51 -0700

    Fixed bug with mysql transactions overlapping and crashing app, changed to trigger script every 5 minutes

--- summaryScripts/main.py
+++ summaryScripts/main.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 import mysql.connector
 from environs import Env
 import time
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 import csv
 import summary_builder
 import query_builder
@@ -20,41 +20,42 @@ mysql_port = env.str("MYSQL_PORT")
 mysql_user = env.str("MYSQL_USER")
 mysql_pw = env.str("MYSQL_PW")
 
-# Mongo DB Connection
-mongo = MongoClient(mongo_uri)
-mongo_db = mongo["test"]
-collection = mongo_db["logs"]
-
-# MySQL Connection
-
-sql = mysql.connector.connect(
-    host=cluster,
-    user=mysql_user,
-    password=mysql_pw,
-    port=mysql_port,
-    collation="utf8mb4_general_ci",
-    database="maria_appDB"
-)
-
-if sql.is_connected():
-    print("Connected to MySQL")
-
-cursor = sql.cursor()
 
 # while loop that will only trigger every hour
 while True:
-    now = datetime.now()
+    now = datetime.now(timezone.utc)
 
-    next_hour = (now + timedelta(hours=1)).replace(minute=0, second=0, microsecond=0)
-    sleep_time = (next_hour - now).total_seconds()
+    next_task = (now + timedelta(minutes=5))
+    sleep_time = (next_task - now).total_seconds()
 
     time.sleep(sleep_time)
 
-    #test summary builder
+    #establish mongo connection
+    # Mongo DB Connection
+    mongo = MongoClient(mongo_uri)
+    mongo_db = mongo["test"]
+    collection = mongo_db["logs"]
+
+    #summary builder
     docs = collection.find({})
     summaries = summary_builder.build_summaries(docs)
 
-    #test query builder
+    #establish connection and cursor
+    sql = mysql.connector.connect(
+        host=cluster,
+        user=mysql_user,
+        password=mysql_pw,
+        port=mysql_port,
+        collation="utf8mb4_general_ci",
+        database="maria_appDB"
+    )
+
+    if sql.is_connected():
+        print("Connected to MySQL")
+
+    cursor = sql.cursor()
+
+    #query builder
     try:
         sql.start_transaction()
 
@@ -66,20 +67,22 @@ while True:
         cursor.execute(transacton_query, transaction_data)
         cursor.execute(fraud_query, fraud_data)
 
+        cursor.execute("SELECT * FROM transaction_summaries")
+        res = cursor.fetchall()
+        for row in res:
+            print(row)
+
         sql.commit()
         print("INSERT successful")
         
     except mysql.connector.Error as err:
         print(f"Error: {err}")
         sql.rollback()
-
-    cursor.execute("SELECT * FROM transaction_summaries")
-    res = cursor.fetchall()
-    for row in res:
-        print(row)
+    
+    finally:
+        cursor.close()
+        sql.close()
+        mongo.close()
 
     print("Executed at: " + str(datetime.now()))
-
-cursor.close()
-sql.close()
 
