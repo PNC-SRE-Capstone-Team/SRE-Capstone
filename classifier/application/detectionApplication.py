4444205f36e8cce23764a9ff3b632684918b8fd7
Damdiel <33411822+Damdiel@users.noreply.github.com>
2024-11-20 11:17:53 -0700

    kafka controller service

--- classifier/application/detectionApplication.py
+++ classifier/application/detectionApplication.py
@@ -19,7 +19,7 @@ def main():
 
     # Kafka consumer setup
     consumer = Consumer({
-    'bootstrap.servers': 'kafka.kafka.svc.cluster.local:9092',
+    'bootstrap.servers': 'kafka-controller-0-external.kafka.svc.cluster.local:9094',
     'group.id': 'fraud-detector',
     'auto.offset.reset': 'earliest'
     })
