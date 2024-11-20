9b66600436db3db7b44bff883aadbd31de36358e
Damdiel <33411822+Damdiel@users.noreply.github.com>
2024-11-20 11:12:38 -0700

    test app

--- classifier/application/detectionApplication.py
+++ classifier/application/detectionApplication.py
@@ -5,53 +5,59 @@ from pymongo import MongoClient
 import logging
 import os
 
-#init mongo uri
-mongo_uri = os.getenv("MONGO_URI")
+def main():
+    #init mongo uri
+    mongo_uri = os.getenv("MONGO_URI")
 
-# Load the model
-model = joblib.load('model.joblib')
+    # Load the model
+    model = joblib.load('model.joblib')
 
-#Init logs
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+    #Init logs
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-logging.info("Fraud Detection App Started Successfully!")
+    logging.info("Fraud Detection App Started Successfully!")
 
-# Kafka consumer setup
-consumer = Consumer({
+    # Kafka consumer setup
+    consumer = Consumer({
     'bootstrap.servers': 'kafka-controller-headless.kafka.svc.cluster.local:9092',
     'group.id': 'fraud-detector',
     'auto.offset.reset': 'earliest'
-})
+    })
 
 
 
-# MongoDB connection
-client = MongoClient(mongo_uri)
-db = client.test
-collection = db.logs
+    # MongoDB connection
+    client = MongoClient(mongo_uri)
+    db = client.test
+    collection = db.logs
 
-# Process Kafka messages
-while True:
-    consumer.subscribe(['transactions'])
-    msg = consumer.poll(1.0)
-    if msg is None:
-        continue
-    if msg.error():
-        print(f"Consumer error: {logging.error(msg.error())}")
-        continue
+    # Process Kafka messages
+    while True:
+        consumer.subscribe(['transactions'])
+        logging.info("loop")
+        msg = consumer.poll(1.0)
+        if msg is None:
+            continue
+        if msg.error():
+            print(f"Consumer error: {logging.error(msg.error())}")
+            continue
 
-    #Transform the incoming msg into a parsable json
-    transaction = json.loads(msg.value().decode('utf-8'))
-    logging.info(transaction)
-    #features = preprocess_transaction(transaction)  # Implement this function
-    #prediction = model.predict([features])[0]
-    #transaction['Fraud'] = int(prediction)
+        logging.info()
 
-    #logging.info(transaction)
+        #Transform the incoming msg into a parsable json
+        transaction = json.loads(msg.value().decode('utf-8'))
+        logging.info(transaction)
+        #features = preprocess_transaction(transaction)  # Implement this function
+        #prediction = model.predict([features])[0]
+        #transaction['Fraud'] = int(prediction)
 
-    # Store in MongoDB
-    #collection.insert_one(transaction)
+        #logging.info(transaction)
 
-    consumer.close()
+        # Store in MongoDB
+        #collection.insert_one(transaction)
+
+        consumer.close()
 
 
+if __name__ == "__main__":
+    main()
